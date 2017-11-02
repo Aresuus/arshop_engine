89eb5a039dd9dd882fb6ab3cf3f0704593c51e7e
owonnarr <owonnarr@gmail.com>
2017-11-02 14:57:34 +0200

    install node and create laravel mix css

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Dashboard Shop-Engine</title>
-    @include('dashboard.partials.style')
+    <link rel="stylesheet" href="/css/dashboard.css">
+    <link rel="stylesheet" href="/dashboard-style/bower_components//font-awesome/css/font-awesome.css">
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 
@@ -13,5 +14,6 @@
     </div>
 
     @include('dashboard.partials.js-scripts')
+    <script src="/js/dashboard.js"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+var mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,36 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+mix.styles([
+    'public/dashboard-style/bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'public/dashboard-style/bower_components//font-awesome/css/font-awesome.css',
+    'public/dashboard-style/bower_components/Ionicons/css/ionicons.min.css',
+    'public/dashboard-style/css/AdminLTE.min.css',
+    'public/dashboard-style/css/skins/_all-skins.min.css',
+    'public/dashboard-style/bower_components/morris.js/morris.css',
+    'public/dashboard-style/bower_components/jvectormap/jquery-jvectormap.css',
+    'public/dashboard-style/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css',
+    'public/dashboard-style/bower_components/bootstrap-daterangepicker/daterangepicker.css',
+], 'public/css/dashboard.css');
+
+// mix.js([
+//     'public/dashboard-style/bower_components/jquery/dist/jquery.min.js',
+//     'public/dashboard-style/bower_components/jquery-ui/jquery-ui.min.js',
+//     'public/dashboard-style/bower_components/bootstrap/dist/js/bootstrap.min.js',
+//     'public/dashboard-style/bower_components/raphael/raphael.min.js',
+//     'public/dashboard-style/bower_components/morris.js/morris.min.js',
+//     'public/dashboard-style/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js',
+//     'public/dashboard-style/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+//     'public/dashboard-style/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+//     'public/dashboard-style/bower_components/jquery-knob/dist/jquery.knob.min.js',
+//     'public/dashboard-style/bower_components/moment/min/moment.min.js',
+//     'public/dashboard-style/bower_components/bootstrap-daterangepicker/daterangepicker.js',
+//     'public/dashboard-style/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
+//     'public/dashboard-style/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+//     'public/dashboard-style/bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
+//     'public/dashboard-style/bower_components/fastclick/lib/fastclick.js',
+//     'public/dashboard-style/js/adminlte.min.js',
+//     'public/dashboard-style/js/demo.js'
+// ], 'public/js/dashboard.js');

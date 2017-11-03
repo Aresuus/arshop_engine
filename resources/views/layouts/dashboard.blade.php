dd3c0dcdbc4e90df241fb697d7442902a281aa5c
dsl <ld@gmail.com>
2017-11-03 18:20:29 +0200

    fix awesome

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Dashboard Shop-Engine</title>
     <link rel="stylesheet" href="/css/dashboard.css">
-    <link rel="stylesheet" href="/dashboard-style/bower_components/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="/font-awesome/fonts/font-awesome.css">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
